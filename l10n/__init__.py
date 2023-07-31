cb1aea1e09fb38db49340043367683da7960ba25
gram <git@orsinium.dev>
2023-07-31 10:38:31 +0200

    do not import entry point in facade

--- l10n/__init__.py
+++ l10n/__init__.py
@@ -2,8 +2,7 @@
 """
 from ._locale import Locale
 from ._locales import Locales
-from ._cli import entrypoint
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __all__ = ['Locales', 'Locale', 'entrypoint']
